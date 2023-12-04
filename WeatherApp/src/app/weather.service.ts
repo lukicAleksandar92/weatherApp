d86c7312222ed4a5f71d391def4bcea696a8a7ce
Aleksandar Lukić <lukicaleksandar1992@gmail.com>
2023-12-04 23:35:02 +0100

    final commit

--- WeatherApp/src/app/weather.service.ts
+++ WeatherApp/src/app/weather.service.ts
@@ -9,7 +9,7 @@ export class WeatherService {
   constructor(private http: HttpClient) { }
 
   getweather(city: string, units: string) {
-    return this.http.get('https://api.openweathermap.org/data/2.5/weather?q=Yakutsk&appid=90d176da6431d184837c7041757254ef&units=metric')
+    return this.http.get('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&{your_Open_API_Key}&units=' + units);
   }
 
 }
